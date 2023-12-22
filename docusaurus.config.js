69e21ec6c0481758f75df65e624e93e4fe2b7bfc
n42rain <ayeng2003@gmail.com>
2023-12-22 15:48:29 +0800

    added smd logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Technical Documentation',
   tagline: 'Knowledge Base',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/smd-logo3.jpeg',
 
   // Set the production url of your site here
   url: 'https://n42rain.github.io',
